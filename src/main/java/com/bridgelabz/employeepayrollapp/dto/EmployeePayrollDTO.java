c6ccf3f7b5430d556d6ccc2c582a10e8f2a00393
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 23:15:36 +0530

    Added: getters and seters

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,10 +3,26 @@ package com.bridgelabz.employeepayrollapp.dto;
 public class EmployeePayrollDTO {
 
     String name;
-    String salary;
+    int salary;
 
-    public EmployeePayrollDTO(String name, String salary) {
+    public EmployeePayrollDTO(String name, int salary) {
         this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
         this.salary = salary;
     }
 
