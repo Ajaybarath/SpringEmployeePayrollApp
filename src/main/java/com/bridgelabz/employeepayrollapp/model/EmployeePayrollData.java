a5a24cb7f9caf27c26fb1b0140aceec4bfca1c81
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 23:16:11 +0530

    Added: object to store employee data

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
+public class EmployeePayrollData {
+
+    int id;
+    String name;
+    int salary;
+
+    public EmployeePayrollData(int id, EmployeePayrollDTO employeePayrollDTO) {
+        this.id = id;
+        this.name = employeePayrollDTO.getName();
+        this.salary = employeePayrollDTO.getSalary();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
