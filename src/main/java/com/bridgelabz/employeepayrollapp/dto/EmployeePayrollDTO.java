e12cf9edab4132b1b8e03ded77bc388c43e79d47
ajaybarath <ajaybarath3@gmail.com>
2021-11-08 13:32:17 +0530

    Added: added for validation for name and salary

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,28 +1,23 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
-public class EmployeePayrollDTO {
+import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+public @Data
+class EmployeePayrollDTO {
+
+    @NotEmpty(message = "Employee cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     String name;
+
+    @Min(value = 500, message = "Min salary should be more than 500")
     int salary;
 
     public EmployeePayrollDTO(String name, int salary) {
         this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
         this.salary = salary;
     }
 
