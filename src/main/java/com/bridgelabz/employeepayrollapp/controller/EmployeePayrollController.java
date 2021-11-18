cadbf0c9448de70bdd07db38934ead600b7512f8
ajaybarath <ajaybarath3@gmail.com>
2021-11-18 08:14:48 +0530

    Added: cors policy to the controller

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
+
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
